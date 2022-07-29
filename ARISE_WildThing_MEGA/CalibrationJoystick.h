15356740e539410bc8f3bf629d43a44e0f5ac89b
tracyofleming <tracyofleming@gmail.com>
2022-07-28 20:10:37 -0400

    Retuned to remove bump.
    Retuned to remove bump on turn, improving mix.

--- ARISE_WildThing_MEGA/CalibrationJoystick.h
+++ ARISE_WildThing_MEGA/CalibrationJoystick.h
@@ -16,19 +16,19 @@ const bool flipSpin_Tether = false; // use true or false to change SPIN directio
 const int numSamples_Tether = 30; // number of samples to check in a "window"
 const int numWindows_Tether = 30; // maximum attempts to check joystick
 
-const float motorDropout = 0.08; // motor dropout ( % of motorMaxSpeed ) Set this to the minimum % it takes to drive the motor.
+const float motorDropout = 0.01; // motor dropout ( % of motorMaxSpeed ) Set this to the minimum % it takes to drive the motor.
 
 
   // Angles
-  const float spinZone      = 20; // number of degrees above or below East or West that one motor turning and other is stopped
+  const float spinZone      = 15; // number of degrees above or below East or West that one motor turning and other is stopped
   const float straightDwell = 0; // degrees travel on either side of forward/backward that is treated as true forward/backward
   const float spinDwell     = 0; // degrees travel on either side of left/right that is treated as true spin
   // Speeds
-  const float motorForward_PowerLevels = 1 ; // range: 0.01 to 1 max FORWARD Speed  (% output for Motor pwm)
-  const float motorReverse_PowerLevels = 0.30 ; // range: 0.01 to 1 max REVERSE Speed (% output for Motor pwm)
-  const float motorTurnFor_PowerLevels = 0.80 ; // range: 0.01 to 1 motor speeds used for FORWARD Turn movement (aka one wheel spins at this speed while other is stopped)  (% output for Motor pwm)
-  const float motorTurnRev_PowerLevels = 0.35 ; // range: 0.01 to 1 motor speeds used for REVERSE Turn movement (aka one wheel spins at this speed while other is stopped)  (% output for Motor pwm)
-  const float motorSpin_PowerLevels = 0.50 ; // range: 0.01 to 1 motor speeds used for Spin movement (aka each motor spins at this speed in opposite directions)  (% output for Motor pwm)
+  const float motorForward_PowerLevels = 1.0 ; // range: 0.01 to 1 max FORWARD Speed  (% output for Motor pwm)
+  const float motorReverse_PowerLevels = 0.75 ; // range: 0.01 to 1 max REVERSE Speed (% output for Motor pwm)
+  const float motorTurnFor_PowerLevels = 1.0 ; // range: 0.01 to 1 motor speeds used for FORWARD Turn movement (aka one wheel spins at this speed while other is stopped)  (% output for Motor pwm)
+  const float motorTurnRev_PowerLevels = 0.75 ; // range: 0.01 to 1 motor speeds used for REVERSE Turn movement (aka one wheel spins at this speed while other is stopped)  (% output for Motor pwm)
+  const float motorSpin_PowerLevels = 0.65 ; // range: 0.01 to 1 motor speeds used for Spin movement (aka each motor spins at this speed in opposite directions)  (% output for Motor pwm)
 
 
 // Radius Lookup Table (Scale)
@@ -39,9 +39,9 @@ const float motorDropout = 0.08; // motor dropout ( % of motorMaxSpeed ) Set thi
 
 // Acceleration & Deceleration Limits
   const float maxAccel = 0.05 ; // range: 0.01 to 1 max acceleration of motor per timestep
-  const float maxDecel = 0.12 ; // range: 0.01 to 1 max deceleration of motor per timestep
-  const float zeroCrossingDwell = 3 ; // number of timesteps (aka loops) to keep motor off between changes in direction (note not "ms", just counting loops)
-  const int BrakeRamp = 100 ; // per loop ramp rate to turn on braking for vnh5019. max braking is 400
+  const float maxDecel = 0.08 ; // range: 0.01 to 1 max deceleration of motor per timestep
+  const float zeroCrossingDwell = 10 ; // number of timesteps (aka loops) to keep motor off between changes in direction (note not "ms", just counting loops)
+  const int BrakeRamp = 10 ; // per loop ramp rate to turn on braking for vnh5019. max braking is 400
 
 // zero crossing variables
   bool motorLForward = true;
